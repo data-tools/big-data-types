1564387b08af5240caceca82e06556438b0e226f
JavierMonton <javier.monton@typeform.com>
2020-11-26 18:25:55 +0100

    Tests

--- src/test/scala/org/datatools/cc2db/bigquery/BigQueryTableSpec.scala
+++ src/test/scala/org/datatools/cc2db/bigquery/BigQueryTableSpec.scala
@@ -6,12 +6,17 @@ class BigQueryTableSpec extends UnitSpec {
 
   behavior of "BigQueryTableSpec"
 
-  val dataset = "tests"
+  //TODO move tests that uses bigquery to Integration tests or mock a BigQuery
+  val dataset = "integration_tests_bigquery"
 
   case class Simple(id: String, version: Int)
 
+  "An error" should "be returned in Left" in {
+    BigQueryTable.createTable[Simple]("nonExistingDataset", "simple").isLeft shouldBe true
+  }
+
   "A Simple Case Class" should "create a table" in {
-    BigQueryTable.createTable[Simple](dataset, "simple")
+    BigQueryTable.createTable[Simple](dataset, "simple").isRight shouldBe true
   }
 
 }
