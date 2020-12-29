33874bf2f1132d180116315d9af5bb8b12d86d35
JavierMonton <javier.monton@typeform.com>
2020-12-29 13:18:16 +0100

    Comments and formats

--- src/main/scala/org/datatools/bigdatatypes/formats/DefaultTransformKeys.scala
+++ src/main/scala/org/datatools/bigdatatypes/formats/DefaultTransformKeys.scala
@@ -4,15 +4,16 @@ trait Formats extends Serializable {
   def transformKeys(s: String): String
 }
 
+/** Default Formats transforms nothing
+  */
 trait DefaultFormats extends Formats {
 
   override def transformKeys(key: String): String = key
 }
 object DefaultFormats extends DefaultFormats
 
-/**
- * Converts CamelCase field names to snake_case
- */
+/** Converts CamelCase field names to snake_case
+  */
 trait SnakifyFormats extends Formats {
 
   override def transformKeys(key: String): String = key
@@ -23,6 +24,8 @@ trait SnakifyFormats extends Formats {
 
 object SnakifyFormats extends SnakifyFormats
 
+/** Formats as implicit vals, it allows to import them instead of declaring a new implicit val
+  */
 object TransformKeys {
   implicit val defaultFormats: Formats = DefaultFormats
   implicit val snakifyFields: Formats = SnakifyFormats
