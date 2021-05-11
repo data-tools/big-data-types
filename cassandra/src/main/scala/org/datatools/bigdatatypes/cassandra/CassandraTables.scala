67fd06eafc0c2935393d9fbb083feda0ee1d3771
JavierMonton <jmlarraz@gmail.com>
2021-05-11 22:41:04 +0200

    WIP SqlTypeToCassandra - CreateTable

--- cassandra/src/main/scala/org/datatools/bigdatatypes/cassandra/CassandraTables.scala
+++ cassandra/src/main/scala/org/datatools/bigdatatypes/cassandra/CassandraTables.scala
@@ -6,28 +6,21 @@ import com.datastax.oss.driver.api.querybuilder.schema.{CreateTable, CreateTable
 
 object CassandraTables {
 
-  def table[A: SqlTypeToCassandra](table: String, primaryKey: String): CreateTable = {
+  /**
+    * Build a CreateTable object with the given Product, table name and primary Key.
+    * If the primary key is not found in the Product, it will be created as Text
+    * @param tableName name of the table
+    * @param primaryKey field name for the primary key, if it doesn't exist, it will be created as Text
+    * @tparam A any Product type
+    * @return CreateTable object
+    */
+  def table[A: SqlTypeToCassandra](tableName: String, primaryKey: String): CreateTable = {
     val tuples: Seq[(String, DataType)] = SqlTypeToCassandra[A].cassandraFields
-    val t: CreateTableStart = createTable(table)
-    val k: CreateTable = t.withPartitionKey("test", DataTypes.TEXT)
+    val t: CreateTableStart = createTable(tableName)
+    val pk = tuples.find(tuple => tuple._1 == primaryKey)
+    //if partition is not found, create it as Text
+    val tmpTable = pk.fold(t.withPartitionKey(primaryKey, DataTypes.TEXT))(tuple => t.withPartitionKey(tuple._1, tuple._2))
     val withoutPk = tuples.dropWhile(tuple => tuple._1 == primaryKey)
-    withoutPk.foldLeft(k) ((tmpTable,tuple) => tmpTable.withColumn(tuple._1, tuple._2))
+    withoutPk.foldLeft(tmpTable)((tmpTable,tuple) => tmpTable.withColumn(tuple._1, tuple._2))
   }
-  /*
-try {
-  val session = CqlSession.builder.build
-  try {
-    val createKs: CreateKeyspace = createKeyspace("cycling").withSimpleStrategy(1)
-    session.execute(createKs.build)
-    val table: CreateTable =
-      createTable("cycling", "cyclist_name")
-        .withPartitionKey("id", DataTypes.UUID)
-        .withColumn("lastname", DataTypes.TEXT)
-        .withColumn("firstname", DataTypes.TEXT)
-    session.execute(createTable.build)
-  }
-  finally if (session != null) session.close()
-}
-
- */
 }
