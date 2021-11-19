a8e0748ca39923f0031ea27b1f2e2cb9edb96535
JavierMonton <javier.monton@typeform.com>
2021-11-19 07:18:26 +0100

    First version of Cassandra Parser

--- cassandra/src/main/scala/org/datatools/bigdatatypes/cassandra/parser/CreateTableParser.scala
+++ cassandra/src/main/scala/org/datatools/bigdatatypes/cassandra/parser/CreateTableParser.scala
@@ -1,46 +1,10 @@
 package org.datatools.bigdatatypes.cassandra.parser
 
-import cats.parse.Rfc5234.sp
+import com.datastax.oss.driver.api.core.`type`.{DataType, DataTypes}
+import com.datastax.oss.driver.api.querybuilder.schema.CreateTable
+import org.datatools.bigdatatypes.cassandra.parser.ParsingError.{ErrorParsingField, ParsingErrors, compactErrors}
 
-object CreateTableParser extends App {
-
-  import cats.data.NonEmptyList
-  import cats.parse.Rfc5234.alpha
-  import cats.parse.Parser
-
-  val p: Parser[String] = alpha.rep.map((l: NonEmptyList[Char]) => l.toList.mkString)
-
-  val p2: Parser[String] = alpha.rep.string
-  val p3: Parser[String] = alpha.repAs[String]
-
-  val parenthesis1: Parser[Unit] = Parser.char('(')
-  val parenthesis2: Parser[Unit] = Parser.char(')')
-  val comma: Parser[Unit] = Parser.char(',')
-
-  val words = (alpha.rep ~ sp.rep0).rep
-  val wordWithComma = (words ~ comma.rep0).rep
-
-  val pk = Parser.string("PRIMARY KEY").rep0
-
-  val test = words ~ parenthesis1 *> pk ~ wordWithComma.string <* pk *> wordWithComma.string <* parenthesis2
-  val fieldsString: Parser[String] = words ~ parenthesis1 *> wordWithComma.string <* parenthesis2
-
-  val fieldsString2 = words ~ parenthesis1 *> pk ~ wordWithComma.string <* pk *> wordWithComma.string <* parenthesis2
-
-  val text =
-    "CREATE TABLE testtable (myint int,mylong bigint PRIMARY KEY,myfloat float,mydouble double,mydecimal decimal,myboolean boolean,mystring text)"
-
-  val a: Either[Parser.Error, Array[String]] = fieldsString.parse(text).map(s => s._2.split(","))
-
-  a match {
-    case Left(value)  => ???
-    case Right(value) => value.foreach(println)
-  }
-  println(fieldsString.parse(text))
-
-}
-
-object Test extends App {
+private[cassandra] object CreateTableParser extends App {
 
   val text =
     "CREATE TABLE testtable (myint int,mylong bigint PRIMARY KEY,myfloat float,mydouble double,mydecimal decimal,myboolean boolean,mystring text)"
@@ -48,16 +12,64 @@ object Test extends App {
   val regex = """\((.*)\)""".r
   val pk = " PRIMARY KEY"
 
-  case class ValueType(value: String, t: String)
+  case class NameType(name: String, t: String)
 
-  val a: Option[Array[ValueType]] = regex
-    .findFirstIn(text)
-    .map(s => s.substring(1).substring(0, s.length - 1).replace(pk, ""))
-    .map(s => s.split(','))
-    .map(l => l.map{s =>
-      val both = s.split(' ')
-      ValueType(both.head, both.tail.head)
-    })
+
+  def parse(table: CreateTable): Either[ParsingError, List[NameType]] = {
+    regex
+      .findFirstIn(table.toString)
+      .map(s => s.substring(1).substring(0, s.length - 1).replace(pk, ""))
+      .map(s => s.split(','))
+      .map(l => l.map { s =>
+        val both = s.split(' ')
+        NameType(both.head, both.tail.head)
+      }.toList) match {
+      case Some(value) => Right(value)
+      case None => Left(ParsingError.ErrorParsingTable("Error parsing CreateTable"))
+    }
+  }
+
+  /**
+    * Given a list of parsed fields, return ParsingErrors or the fields parsed and typed for Cassandra
+    * @param fields is a list of Fields parsed
+    * @return Either with [[ParsingErrors]] or a list of fields with their types
+    */
+  def toCassandraTypes(fields: List[NameType]): Either[ParsingError, Seq[(String, DataType)]] = {
+    val typed: Seq[Either[ParsingError, (String, DataType)]] = fields.map(field => parsedToCassandra(field))
+
+    val err = typed.filter(_.isLeft).map {
+      case Left(value) => value
+    }
+
+    if (err.nonEmpty) {
+      Left(compactErrors(err))
+    }
+    else {
+      val values = typed.map {
+        case Right(value) => value
+      }
+      Right(values)
+    }
+  }
+
+  /**
+    * Given a field parsed and it's type, return a valid Cassandra field name and type, or a Parsing error
+    * @param field a field with name and type
+    * @return Either a parsed type or a [[ParsingError]]
+    */
+  def parsedToCassandra(field: NameType): Either[ParsingError, (String, DataType)] = {
+    field.t.toLowerCase match {
+      case "int" => Right((field.name, DataTypes.INT))
+      case "bigint" => Right((field.name, DataTypes.BIGINT))
+      case "float" => Right((field.name, DataTypes.FLOAT))
+      case "double" => Right((field.name, DataTypes.DOUBLE))
+      case "decimal" => Right((field.name, DataTypes.DECIMAL))
+      case "boolean" => Right((field.name, DataTypes.BOOLEAN))
+      case "text" => Right((field.name, DataTypes.TEXT))
+      case _ => Left(ErrorParsingField(field.name, s"Type ${field.t} not implemented"))
+    }
+  }
+
 
 
 }
