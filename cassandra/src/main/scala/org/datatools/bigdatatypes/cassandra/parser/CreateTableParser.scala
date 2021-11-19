83b48e256a63518a6f61b5bdd5f1dc688830c92f
JavierMonton <javier.monton@typeform.com>
2021-11-19 07:30:06 +0100

    Formatting

--- cassandra/src/main/scala/org/datatools/bigdatatypes/cassandra/parser/CreateTableParser.scala
+++ cassandra/src/main/scala/org/datatools/bigdatatypes/cassandra/parser/CreateTableParser.scala
@@ -2,7 +2,7 @@ package org.datatools.bigdatatypes.cassandra.parser
 
 import com.datastax.oss.driver.api.core.`type`.{DataType, DataTypes}
 import com.datastax.oss.driver.api.querybuilder.schema.CreateTable
-import org.datatools.bigdatatypes.cassandra.parser.ParsingError.{ErrorParsingField, ParsingErrors, compactErrors}
+import org.datatools.bigdatatypes.cassandra.parser.ParsingError.{compactErrors, ErrorParsingField, ParsingErrors}
 
 private[cassandra] object CreateTableParser extends App {
 
@@ -14,62 +14,59 @@ private[cassandra] object CreateTableParser extends App {
 
   case class NameType(name: String, t: String)
 
-
-  def parse(table: CreateTable): Either[ParsingError, List[NameType]] = {
+  /** Given a [[CreateTable]], uses it's representation of String to extract their fields and types
+    * @param table a [[CreateTable]] instance
+    * @return Either a [[ParsingError]] or a list of [[NameType]] with names and types in String
+    */
+  def extractComponents(table: CreateTable): Either[ParsingError, List[NameType]] =
     regex
       .findFirstIn(table.toString)
       .map(s => s.substring(1).substring(0, s.length - 1).replace(pk, ""))
       .map(s => s.split(','))
-      .map(l => l.map { s =>
-        val both = s.split(' ')
-        NameType(both.head, both.tail.head)
-      }.toList) match {
+      .map(l =>
+        l.map { s =>
+          val both = s.split(' ')
+          NameType(both.head, both.tail.head)
+        }.toList
+      ) match {
       case Some(value) => Right(value)
-      case None => Left(ParsingError.ErrorParsingTable("Error parsing CreateTable"))
+      case None        => Left(ParsingError.ErrorParsingTable("Error parsing CreateTable"))
     }
-  }
 
-  /**
-    * Given a list of parsed fields, return ParsingErrors or the fields parsed and typed for Cassandra
+  /** Given a list of parsed fields, return ParsingErrors or the fields parsed and typed for Cassandra
     * @param fields is a list of Fields parsed
     * @return Either with [[ParsingErrors]] or a list of fields with their types
     */
   def toCassandraTypes(fields: List[NameType]): Either[ParsingError, Seq[(String, DataType)]] = {
     val typed: Seq[Either[ParsingError, (String, DataType)]] = fields.map(field => parsedToCassandra(field))
 
-    val err = typed.filter(_.isLeft).map {
-      case Left(value) => value
-    }
+    val err = typed.filter(_.isLeft).map { case Left(value) => value }
 
     if (err.nonEmpty) {
       Left(compactErrors(err))
     }
     else {
-      val values = typed.map {
-        case Right(value) => value
+      val values = typed.map { case Right(value) =>
+        value
       }
       Right(values)
     }
   }
 
-  /**
-    * Given a field parsed and it's type, return a valid Cassandra field name and type, or a Parsing error
+  /** Given a field parsed and it's type, return a valid Cassandra field name and type, or a Parsing error
     * @param field a field with name and type
     * @return Either a parsed type or a [[ParsingError]]
     */
-  def parsedToCassandra(field: NameType): Either[ParsingError, (String, DataType)] = {
+  def parsedToCassandra(field: NameType): Either[ParsingError, (String, DataType)] =
     field.t.toLowerCase match {
-      case "int" => Right((field.name, DataTypes.INT))
-      case "bigint" => Right((field.name, DataTypes.BIGINT))
-      case "float" => Right((field.name, DataTypes.FLOAT))
-      case "double" => Right((field.name, DataTypes.DOUBLE))
+      case "int"     => Right((field.name, DataTypes.INT))
+      case "bigint"  => Right((field.name, DataTypes.BIGINT))
+      case "float"   => Right((field.name, DataTypes.FLOAT))
+      case "double"  => Right((field.name, DataTypes.DOUBLE))
       case "decimal" => Right((field.name, DataTypes.DECIMAL))
       case "boolean" => Right((field.name, DataTypes.BOOLEAN))
-      case "text" => Right((field.name, DataTypes.TEXT))
-      case _ => Left(ErrorParsingField(field.name, s"Type ${field.t} not implemented"))
+      case "text"    => Right((field.name, DataTypes.TEXT))
+      case _         => Left(ErrorParsingField(field.name, s"Error parsing field: Type ${field.t} not implemented"))
     }
-  }
-
-
 
 }
--- cassandra/src/main/scala/org/datatools/bigdatatypes/cassandra/parser/ParsingError.scala
+++ cassandra/src/main/scala/org/datatools/bigdatatypes/cassandra/parser/ParsingError.scala
@@ -3,35 +3,33 @@ package org.datatools.bigdatatypes.cassandra.parser
 //TODO: This could be moved to a new Parsing module when more modules need a parser
 //make in it private for now to avoid breaking changes in the future
 
-private[parser] sealed trait ParsingError
+sealed private[parser] trait ParsingError
 
 private[parser] object ParsingError {
   case class ErrorParsingTable(msg: String) extends ParsingError
   case class ErrorParsingField(fieldName: String, msg: String) extends ParsingError
   case class ParsingErrors(errors: List[ParsingError]) extends ParsingError
 
-  /**
-    * Given a list of errors, transform them into a [[ParsingErrors]]
+  /** Given a list of errors, transform them into a [[ParsingErrors]]
     * This could be useful when we want to accumulate all parsing errors
     * @param errors a list of [ParsingError]
     * @return [[ParsingError]] with all errors inside
     */
   def compactErrors(errors: Seq[ParsingError]): ParsingErrors = {
-    val acc: ParsingErrors  = ParsingErrors(List.empty[ParsingError])
+    val acc: ParsingErrors = ParsingErrors(List.empty[ParsingError])
     errors.foldLeft(acc)((e1, e2) => ParsingErrors(e1.errors :+ e2))
   }
 
   implicit class ExtensionForError(error: ParsingError) {
-    /**
-      * Add an error to a [[ParsingError]]. It converts the error into a [[ParsingErrors]]
+
+    /** Add an error to a [[ParsingError]]. It converts the error into a [[ParsingErrors]]
       * @param newError the error to be added
       * @return [[ParsingErrors]] with current errors + the new one
       */
-    def addError(newError: ParsingError): ParsingErrors = {
+    def addError(newError: ParsingError): ParsingErrors =
       error match {
         case ParsingErrors(errors) => ParsingErrors(errors :+ newError)
-        case _ => ParsingErrors(List(newError))
+        case _                     => ParsingErrors(List(newError))
       }
-    }
   }
 }
